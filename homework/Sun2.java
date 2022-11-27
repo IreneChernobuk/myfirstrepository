1d743c230a0b0de338b35863c1d61f36d0a89a64
Irine Chernobuk <irinka_buc@mail.ru>
2022-11-27 21:26:37 +0300

    homework about sun. my second version

---
+++ homework/Sun2.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class Sun2 {
+    public static int SunCount = 0;
+
+    public static void Sunrise() {
+        SunCount++;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SunCount);
+    }
+}
